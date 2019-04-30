410782e817477c53172b28d51954fbbe178dcf32
Justin Welsch <j.welsch@gmail.com>
2019-04-30 11:09:24 -0400

    Restructured directories and added test project

---
+++ ApplicationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ApplicationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ApplicationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2276339d-3fe7-48c9-9d9e-b4b93ea43e0c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/DealerOnProblemOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealerOnProblemOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to NASA's Rover Operation Command Interpreter (ROCI).");
+
+            while (true)
+            {
+                Console.Write("Please enter a path to a command file: ");
+                var path = Console.ReadLine();
+                Console.WriteLine();
+
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine($"The file does not exist at path {path}.");
+                    continue;
+                }
+
+
+            }
+        }
+    }
+}
