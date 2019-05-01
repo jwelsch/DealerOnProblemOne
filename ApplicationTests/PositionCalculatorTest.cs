ac630d4e2b281cbf8108439db7442e1a0f69921b
Justin Welsch <j.welsch@gmail.com>
2019-05-01 12:04:21 -0400

    Add PositionCalculator tests

---
+++ ApplicationTests/PositionCalculatorTest.cs
@@ -0,0 +1,191 @@
+﻿using DealerOnProblemOne;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTests
+{
+    [TestClass]
+    public class PositionCalculatorTest
+    {
+        [TestMethod]
+        public void MoveOnNorthHeadingFromPositiveYAxisShouldReturnPositiveYAxis()
+        {
+            var heading = Heading.North;
+            var start = new Point(2, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(2, 3), result);
+        }
+
+        [TestMethod]
+        public void MoveOnNorthHeadingFromNegativeYAxisShouldReturnNegativeYAxis()
+        {
+            var heading = Heading.North;
+            var start = new Point(2, -2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(2, -1), result);
+        }
+
+        [TestMethod]
+        public void MoveOnNorthHeadingFromNegativeYAxisShouldReturnZeroYAxis()
+        {
+            var heading = Heading.North;
+            var start = new Point(2, -1);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(2, 0), result);
+        }
+
+        [TestMethod]
+        public void MoveOnNorthHeadingFromZeroYAxisShouldReturnPositiveYAxis()
+        {
+            var heading = Heading.North;
+            var start = new Point(2, 0);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(2, 1), result);
+        }
+
+        [TestMethod]
+        public void MoveOnEastHeadingFromPositiveXAxisShouldReturnPositiveXAxis()
+        {
+            var heading = Heading.East;
+            var start = new Point(2, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(3, 2), result);
+        }
+
+        [TestMethod]
+        public void MoveOnEastHeadingFromNegativeXAxisShouldReturnNegativeXAxis()
+        {
+            var heading = Heading.East;
+            var start = new Point(-2, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(-1, 2), result);
+        }
+
+        [TestMethod]
+        public void MoveOnEastHeadingFromNegativeXAxisShouldReturnZeroXAxis()
+        {
+            var heading = Heading.East;
+            var start = new Point(-1, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(0, 2), result);
+        }
+
+        [TestMethod]
+        public void MoveOnEastHeadingFromZeroXAxisShouldReturnPositiveXAxis()
+        {
+            var heading = Heading.East;
+            var start = new Point(0, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(1, 2), result);
+        }
+
+        [TestMethod]
+        public void MoveOnSouthHeadingFromPositiveYAxisShouldReturnPositiveYAxis()
+        {
+            var heading = Heading.South;
+            var start = new Point(2, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(2, 1), result);
+        }
+
+        [TestMethod]
+        public void MoveOnSouthHeadingFromNegativeYAxisShouldReturnNegativeYAxis()
+        {
+            var heading = Heading.South;
+            var start = new Point(2, -2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(2, -3), result);
+        }
+
+        [TestMethod]
+        public void MoveOnSouthHeadingFromPositiveYAxisShouldReturnZeroYAxis()
+        {
+            var heading = Heading.South;
+            var start = new Point(2, 1);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(2, 0), result);
+        }
+
+        [TestMethod]
+        public void MoveOnSouthHeadingFromZeroYAxisShouldReturnNegativeYAxis()
+        {
+            var heading = Heading.South;
+            var start = new Point(2, 0);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(2, -1), result);
+        }
+
+        [TestMethod]
+        public void MoveOnWestHeadingFromPositiveXAxisShouldReturnPositiveXAxis()
+        {
+            var heading = Heading.West;
+            var start = new Point(2, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(1, 2), result);
+        }
+
+        [TestMethod]
+        public void MoveOnWestHeadingFromNegativeXAxisShouldReturnNegativeXAxis()
+        {
+            var heading = Heading.West;
+            var start = new Point(-2, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(-3, 2), result);
+        }
+
+        [TestMethod]
+        public void MoveOnWestHeadingFromPositiveXAxisShouldReturnZeroXAxis()
+        {
+            var heading = Heading.West;
+            var start = new Point(1, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(0, 2), result);
+        }
+
+        [TestMethod]
+        public void MoveOnWestHeadingFromZeroXAxisShouldReturnNegativeXAxis()
+        {
+            var heading = Heading.West;
+            var start = new Point(0, 2);
+
+            var result = PositionCalculator.Move(heading, start);
+
+            Assert.AreEqual(new Point(-1, 2), result);
+        }
+    }
+}
--- Source/DealerOnProblemOne/PositionCalculator.cs
+++ Source/DealerOnProblemOne/PositionCalculator.cs
@@ -9,18 +9,18 @@ namespace DealerOnProblemOne
 {
     public static class PositionCalculator
     {
-        public static Point Move(Heading heading, Point position)
+        public static Point Move(Heading heading, Point startPosition)
         {
             switch (heading)
             {
                 case Heading.North:
-                    return new Point(position.X, position.Y + 1);
+                    return new Point(startPosition.X, startPosition.Y + 1);
                 case Heading.South:
-                    return new Point(position.X, position.Y - 1);
+                    return new Point(startPosition.X, startPosition.Y - 1);
                 case Heading.East:
-                    return new Point(position.X + 1, position.Y);
+                    return new Point(startPosition.X + 1, startPosition.Y);
                 case Heading.West:
-                    return new Point(position.X - 1, position.Y);
+                    return new Point(startPosition.X - 1, startPosition.Y);
                 default:
                     throw new ArgumentException($"Unknown heading {heading} specified.", nameof(heading));
             }
